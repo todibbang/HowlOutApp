71ddcfec1e1679d6b09021399a03292901f65fce
todibbang <todibbang@gmail.com>
2016-02-14 23:12:30 +0100

    create event changes

--- HowlOut/Views/CreateEvent.xaml.cs
+++ HowlOut/Views/CreateEvent.xaml.cs
@@ -6,10 +6,57 @@ namespace HowlOut
 {
 	public partial class CreateEvent : ContentView
 	{
+		string Title;
+		string Description;
+		string StartTime;
+		string EndTime;
+		string StartDate;
+		string EndDate;
+		string MinAge;
+		string MaxAge;
+		string MinSize;
+		string MaxSize;
+
+
 		public CreateEvent ()
 		{
 			InitializeComponent ();
+
+
+			title.TextChanged += (sender, e) => {
+				Title = title.Text;
+			};
+			description.TextChanged += (sender, e) => {
+				Description = description.Text;
+			};
+
+			startTime.PropertyChanged += (sender, e) => {
+				StartTime = startTime.Time.ToString();
+			};
+			endTime.PropertyChanged += (sender, e) => {
+				EndTime = endTime.Time.ToString();
+			};
+			startDate.PropertyChanged += (sender, e) => {
+				StartDate = startDate.Date.ToString();
+			};
+			endDate.PropertyChanged += (sender, e) => {
+				EndDate = endDate.Date.ToString();
+			};
+			minAge.TextChanged += (sender, e) => {
+				MinAge = minAge.Text;
+			};
+			maxAge.TextChanged += (sender, e) => {
+				MaxAge = maxAge.Text;
+			};
+			minSize.Completed += (sender, e) => {
+				MinSize = minSize.Text;
+			};
+			maxSize.Completed += (sender, e) => {
+				MaxSize = maxSize.Text;
+			};
+
 		}
+
 	}
 }
 
