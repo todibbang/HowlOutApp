8ffe313e2f509d616f5438d33a1d89fea6537c10
todibbang <todibbang@gmail.com>
2016-02-28 20:56:07 +0100

    userProfile

--- HowlOut/Views/TopBar.xaml.cs
+++ HowlOut/Views/TopBar.xaml.cs
@@ -32,7 +32,6 @@ namespace HowlOut
 			{
 				dataManager.updateLists();
 			};
-
 		}
 	}
 }
---
+++ HowlOut/Views/UserProfile.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HowlOut
+{
+	public partial class UserProfile : ContentView
+	{
+		public UserProfile ()
+		{
+			InitializeComponent ();
+
+			Likes.Text = "2";
+			Loyalty.Text = "100%";
+			NameAndAge.Text = "Name and Age";
+
+			DataManager dataManager = new DataManager();
+			var profilePicUri = dataManager.GetFacebookProfileImageUri(App.StoredUserFacebookId);
+			Image.Source = ImageSource.FromUri(profilePicUri);
+		}
+	}
+}
+
