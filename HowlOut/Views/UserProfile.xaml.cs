6914aa8557ffb9fcf7c0b595d9f0a7f1fd9c3868
todibbang <todibbang@gmail.com>
2016-03-07 16:50:38 +0100

    chsngs

--- HowlOut/Views/UserProfile.xaml.cs
+++ HowlOut/Views/UserProfile.xaml.cs
@@ -13,6 +13,7 @@ namespace HowlOut
 		Profile userProfile;
 		Group userGroup;
 		Event eventObject;
+		bool InviteMode = false;
 
 		DataManager dataManager = new DataManager();
 		private EventApiManager eventApiManager;
@@ -24,9 +25,13 @@ namespace HowlOut
 		ObservableCollection <Group> groupsToInvite = new ObservableCollection <Group>();
 		public CreateEvent createEventView;
 
+		Button friendRequestButton = new Button ();
+		Button groupRequestButton = new Button ();
+
 		public UserProfile (Profile prof, Group group, Event eve, bool inviteMode, bool observer)
 		{
 			InitializeComponent ();
+			InviteMode = inviteMode;
 
 			httpClient = new HttpClient(new NativeMessageHandler());
 			eventApiManager = new EventApiManager (httpClient);
@@ -42,30 +47,37 @@ namespace HowlOut
 			List<Comment> givenList = new List<Comment> ();
 			if(inviteMode){
 				infoView.IsVisible = false;
-				List<Profile> profilesToInvite = App.userProfile.Friends;
 
-				Dictionary<Profile, string> profilesNotToInvite = new Dictionary<Profile, string> { };
+				if (userProfile == null) {
+					List<Profile> profilesToInvite = App.userProfile.Friends;
+					Dictionary<Profile, string> profilesNotToInvite = new Dictionary<Profile, string> { };
+					if (userGroup != null) { 
+						for (int e = 0; e < userGroup.Members.Count; e++) {
+							profilesNotToInvite.Add (userGroup.Members [e], userGroup.Members [e].ProfileId);
+						}
+					} else if (eventObject != null) { 
+						for (int e = 0; e < eventObject.Attendees.Count; e++) {
+							profilesNotToInvite.Add (eventObject.Attendees [e], eventObject.Attendees [e].ProfileId);
+						}
+					}
 
-				if (userGroup != null) { 
-					for (int e = 0; e < userGroup.Members.Count; e++) {
-						profilesNotToInvite.Add (userGroup.Members[e], userGroup.Members[e].ProfileId);
+					for (int i = profilesToInvite.Count - 1; i > -1; i--) {
+						if (profilesNotToInvite.ContainsValue (profilesToInvite [i].ProfileId)) {
+							System.Diagnostics.Debug.WriteLine ("Catch");
+							profilesToInvite.Remove (profilesToInvite [i]);
+						}
 					}
-				} else if (eventObject != null) { 
-					for (int e = 0; e < eventObject.Attendees.Count; e++) {
-						profilesNotToInvite.Add (eventObject.Attendees[e], eventObject.Attendees[e].ProfileId);
-					}
+					createList (profilesToInvite , null, profileGrid);
+					createList (null, App.userProfile.Groups, groupGrid);
+					wallButton.IsVisible = false;
+				} else {
+					createList (userProfile.RecievedFriendRequests , null, profileGrid);
+					createList (null, App.userProfile.GroupsInviteTo, groupGrid);
+					wallButton.IsVisible = false;
+					inviteLayout.IsVisible = false;
 				}
 
-				for(int i = profilesToInvite.Count - 1; i > -1; i--) {
-					if (profilesNotToInvite.ContainsValue (profilesToInvite [i].ProfileId)) {
-						System.Diagnostics.Debug.WriteLine ("Catch");
-						profilesToInvite.Remove (profilesToInvite [i]);
-					}
-				}
 
-				createList (profilesToInvite , null, profileGrid);
-				createList (null, App.userProfile.Groups, groupGrid);
-				wallButton.IsVisible = false;
 			} else {
 				inviteLayout.IsVisible = false;
 				if (userProfile != null) {
@@ -119,16 +131,26 @@ namespace HowlOut
 			foreach (Button button in friendButtons) {
 				button.Clicked += (sender, e) => {
 					Profile profile = null;
-					if (userProfile != null) { profile = userProfile.Friends[int.Parse(button.Text)];} 
+					if (userProfile != null) { 
+						if(InviteMode)
+						{
+							profile = userProfile.RecievedFriendRequests[int.Parse(button.Text)];
+						} else {
+							profile = userProfile.Friends[int.Parse(button.Text)];
+						}
+						System.Diagnostics.Debug.WriteLine(button.Text);
+
+					} 
 					else if(userGroup != null) { profile = userGroup.Members[int.Parse(button.Text)];} 
 					else if(eventObject != null) { profile = eventObject.Attendees[int.Parse(button.Text)];}
 
-					if(inviteMode){
+					if(inviteMode && userProfile == null){
 						if(!friendsToInvite.Contains(profile)) {
 							friendsToInvite.Add(profile);
 						}
 					}
 					else {
+						System.Diagnostics.Debug.WriteLine("This ? Button Pressed");
 						App.coreView.setContentView (new UserProfile (profile, null, null, false, false), "UserProfile");
 					}
 				};
@@ -154,6 +176,15 @@ namespace HowlOut
 					sendInviteToEvent();
 				}
 			};
+
+			friendRequestButton.Clicked += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("This ? Freind Request Button Pressed");
+				App.coreView.setContentView (new UserProfile (userProfile, null, null, true, false), "UserProfile");
+			};
+
+			groupRequestButton.Clicked += (sender, e) => {
+
+			};
 		}
 
 
@@ -164,6 +195,22 @@ namespace HowlOut
 				count = profiles.Count;
 			} else {
 				count = groups.Count;
+			}
+
+			bool addRequestButton = false;
+			//if (row == 0 && column == 0 && userProfile != null && userProfile.ProfileId == App.userProfile.ProfileId) {
+			if (userProfile != null && !InviteMode) {
+				if (profiles != null) {
+					if (userProfile.RecievedFriendRequests.Count != 0) {
+						count++;
+						addRequestButton = true;
+					}
+				} else if (groups != null) {
+					if (userProfile.GroupsInviteTo.Count != 0) {
+						count++;
+						addRequestButton = true;
+					}
+				}
 			}
 
 			Grid newGrid = new Grid {
@@ -181,6 +228,7 @@ namespace HowlOut
 				ColumnSpacing=0,
 			};
 
+			int subjectNr = 0;
 			int column = 0;
 			int row = 0;
 
@@ -194,22 +242,58 @@ namespace HowlOut
 				Grid cell = new Grid ();
 				Button button = new Button ();
 
-				if (profiles != null) {
-					cell = friendListCreator (profiles [i]);
-					button = buttonCreator (i);
-					friendButtons.Add (button);
+				//if (row == 0 && column == 0 && userProfile != null && userProfile.ProfileId == App.userProfile.ProfileId) {
+				if (addRequestButton) {
+					if (profiles != null) {
+						if (userProfile.RecievedFriendRequests.Count != 0) {
+							cell = requestButton(userProfile.RecievedFriendRequests.Count);
+							button = buttonCreator(0);
+							friendRequestButton = button;
+							subjectNr--;
+							System.Diagnostics.Debug.WriteLine ("success!!!!!!!");
+						}
+					} else if (groups != null) {
+						if (userProfile.GroupsInviteTo.Count != 0) {
+							cell = requestButton (userProfile.GroupsInviteTo.Count);
+							button = buttonCreator(0);
+							groupRequestButton = button;
+							subjectNr--;
+						}
+					}
+				}
 
-				} else if (groups != null) {
-					cell = groupListCreator (groups [i]);
-					button = buttonCreator (i);
-					groupButtons.Add (button);
+				if (addRequestButton != true) {
+					if (profiles != null) {
+						cell = friendListCreator (profiles [subjectNr]);
+						button = buttonCreator (subjectNr);
+						friendRequestButton = button;
+						friendButtons.Add (button);
+
+					} else if (groups != null) {
+						cell = groupListCreator (groups [subjectNr]);
+						button = buttonCreator (subjectNr);
+						groupRequestButton = button;
+						groupButtons.Add (button);
+					}
 				}
 				//adds the whole cell to the grid
 				grid.Children.Add (cell, column, row);
 				grid.Children.Add (button, column, row);
 
 				column ++;
+				subjectNr++;
 			}
+			/*
+			for (int i = subjectNr; i < 3; i++) {
+				if (column == 3) {
+					column = 0;
+					row++;
+					grid.RowDefinitions.Add (new RowDefinition{ Height = 150 });
+				}
+				grid.Children.Add (buttonCreator(0), column, row);
+				column ++;
+			}
+			*/
 
 			grid = newGrid;
 		}
@@ -259,6 +343,34 @@ namespace HowlOut
 				BackgroundColor = Color.Transparent,
 			};
 			return button;
+		}
+
+		private Grid requestButton(int i)
+		{
+			Grid cellGrid = new Grid {
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				RowDefinitions = {
+					new RowDefinition{ Height = 10 },
+					new RowDefinition{ Height = 85 },
+					new RowDefinition{ Height = 40 },
+				}
+			};
+
+			Button groupButton = new Button {
+				HeightRequest = 85,
+				WidthRequest = 85,
+				BorderRadius = 42,
+				BorderWidth = 6,
+				BackgroundColor = Color.White,
+				HorizontalOptions = LayoutOptions.Center,
+				Text = i + "",
+				BorderColor = Color.Red
+			};
+
+			cellGrid.Children.Add (groupButton, 0,1);
+
+			return cellGrid;
 		}
 
 		private Grid groupListCreator(Group group)
