3c4e81a3e92966419bb117fc61f9f8a64dc93062
William Jensen <wjaugustine@gmail.com>
2016-03-21 02:45:13 +0100

    Added local notifications

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -13,7 +13,15 @@ namespace HowlOut.iOS
 	{
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			global::Xamarin.Forms.Forms.Init ();
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(
+                    UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                    new NSSet());
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            global::Xamarin.Forms.Forms.Init ();
 
 			ImageCircleRenderer.Init ();
 
