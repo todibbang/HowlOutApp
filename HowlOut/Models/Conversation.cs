22e43dc69b0e89ee0580f4157ab70c0623fc9829
todibbang <macbookpro@Macbooks-MBP.lan>
2016-10-06 19:12:05 +0200

    Conversation Implementation

---
+++ HowlOut/Models/Conversation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowlOut
+{
+	public class Conversation
+	{
+		public int ConversationID { get; set; }
+		public List <Profile> profiles { get; set; }
+		public List <Comment> comments { get; set; }
+		public DateTime lastUpdated { get; set; }
+
+		public Conversation()
+		{
+		}
+	}
+}
